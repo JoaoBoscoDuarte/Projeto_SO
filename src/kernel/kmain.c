7d6691d4723bdfc69ef1d184b64f799e47b8cdba
Marcus-Vin <marcusvc703@gmail.com>
2026-03-11 00:53:28 -0300

    fix: update kprintf to log result of bitwise AND with module count check in kmain function

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -41,7 +41,7 @@ void kmain(unsigned int ebx) {
     // 6. Testa kprintf com formatação
     // OUTPUT_FB: envia apenas para framebuffer (tela)
     // %d: formata inteiro decimal
-    kprintf(OUTPUT_FB, "bitwise AND: %d\n", flags & MULTIBOOT_INFO_MODS);
+    kprintf(OUTPUT_FB, "bitwise AND result: %d\n", (flags & MULTIBOOT_INFO_MODS) && number_of_modules > 0);
     kprintf(OUTPUT_FB, "\nFlags: 0x%x\n", flags);
     kprintf(OUTPUT_FB, "Endereco da lista de modulos: 0x%x\n", (unsigned int) mbinfo->mods_addr);
     kprintf(OUTPUT_FB, "Numero de modulos: %d\n", number_of_modules);
